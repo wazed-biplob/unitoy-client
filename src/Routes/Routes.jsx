2a0a397096a744759096b413956608df6eabedea
Wazed Biplob <www.biplob@gmail.com>
2023-05-17 23:31:24 +0600

    Basic Folder Structure Completed

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import { Outlet, createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import { Children } from "react";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
