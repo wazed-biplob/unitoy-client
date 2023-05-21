85c8d4c2bc9fcecf834d89794de4338019994cad
Wazed Biplob <www.biplob@gmail.com>
2023-05-21 14:09:13 +0600

    404 Route Completed

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>404</h1>
+    <div
+      style={{
+        width: "calc(100vw - 40px)",
+        height: "calc(100vh - 20px)",
+        backgroundImage: `url('https://i.ibb.co/4fVXFKm/Untitled.jpg')`,
+        backgroundSize: "100% 100%",
+      }}
+      className="flex absolute justify-end items-center mx-auto"
+    >
+      <div className="w-1/2 h-full relative flex flex-col justify-center items-center p-8 gap-8">
+        <h1 className="text-5xl font-extrabold">Oops :( 404</h1>
+        <p className="text-4xl font-bold">Looks Like your are lost!</p>
+        <p>Maybe you have tried to visited a place which does not exist!!!</p>
+        <Link to="/">
+          <button className="btn">Take Me Home</button>
+        </Link>
+      </div>
     </div>
   );
 };
